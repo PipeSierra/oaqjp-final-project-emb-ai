7a40a81b32735769595eef269cc8b3269c39a6b6
PipeSierra <pipesierra@outlook.com>
2026-09-11 23:47:19 -0500

    Add files via upload

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,20 @@
+import requests 
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    jsonObj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = jsonObj, headers = header) 
+        
+    if response.status_code == 200:
+        formatted_text = json.loads(response.text)
+        emotions = formatted_text['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions, key=emotions.get)
+        emotions['dominant_emotion'] = dominant_emotion
+        
+        return emotions
+    
+    else: # response.status_code == 400 / 500
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None }
+
---
+++ server.py
@@ -0,0 +1,34 @@
+"""Server file that act as API to serve the UI the data"""
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector")
+def emotion_detector_endpoint():
+    """Generate a template for the UI with the data that emotion detector returens.
+
+    Returns:
+        template: the result after analizing the emotion.
+    """
+
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
+    return (f"For the given statement, the system response is 'anger': {response['anger']},"
+        f"'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} "
+        f"and 'sadness': { response['sadness']}. "
+        f"The dominant emotion is {response['dominant_emotion']}.")
+
+@app.route("/")
+def render_index_page():
+    """Renders the main/index html page
+    """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
